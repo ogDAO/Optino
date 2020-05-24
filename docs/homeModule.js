b2a1b97cc7ffa69408e41e962bb72dffdd1c8fd1
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-05-24 14:35:52 +1000

    UI WIP

--- docs/homeModule.js
+++ docs/homeModule.js
@@ -3,22 +3,17 @@ const Home = {
     <div class="mt-5 pt-3">
       <b-row>
         <b-col cols="12" md="9" class="m-0 p-1">
-          <b-card header="Home" class="border-0" header-class="p-1">
-            <b-list-group flush>
-              <b-list-group-item to="/optinoExplorer/all">Optino Explorer</b-list-group-item>
-              <b-list-group-item to="/tokensExplorer/all">Tokens Explorer</b-list-group-item>
-              <b-list-group-item to="/feedsExplorer/all">Feeds Explorer</b-list-group-item>
-              <!--
-              <b-list-group-item to="/tokenContractExplorer/all">Token Contract Explorer</b-list-group-item>
-              <b-list-group-item to="/dataServiceExplorer/all">Data Service Explorer</b-list-group-item>
-              <b-list-group-item to="/ipfsExplorer/all">IPFS Explorer</b-list-group-item>
-              <b-list-group-item to="/apiReference/all">API Reference</b-list-group-item>
-              -->
-            </b-list-group>
-            <br />
-            <b-card-text>
-              Note that you will need a browser with a web3 injection, e.g., using the MetaMask addon. In your web3 wallet, switch to the Ropsten testnet.
-            </b-card-text>
+          <b-card no-body header="Home" class="border-0" header-class="p-1">
+            <b-card-body class="p-1 mt-5">
+              <b-list-group>
+                <b-list-group-item to="/optinoExplorer/all">Optino Explorer</b-list-group-item>
+                <b-list-group-item to="/tokensExplorer/all">Tokens Explorer</b-list-group-item>
+                <b-list-group-item to="/feedsExplorer/all">Feeds Explorer</b-list-group-item>
+              </b-list-group>
+              <b-card-text class="mt-5">
+                Note that you will need a browser with a web3 injection, e.g., using the MetaMask addon. In your web3 wallet, switch to the Ropsten testnet.
+              </b-card-text>
+            </b-card-body>
           </b-card>
 
         </b-col>
